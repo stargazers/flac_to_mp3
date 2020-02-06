e4c9e07ed79aebc49bf1e3f5c8b0505a887c00e5
Aleksi Räsänen <aleksi.rasanen@runosydan.net>
2020-02-06 22:29:21 +0200

    Support for output folder

--- lame.py
+++ lame.py
@@ -7,13 +7,34 @@ import subprocess
 
 # Where we look for FLAC files
 flac_watchfolder = '/share2/Audio/flac_to_mp3/'
-output_folder = '/share2/Aduio/mp3/'
+output_folder = '/share2/Audio/mp3/'
+
+print("****************************************")
+print(" FLAC to MP3 conversion")
+print("****************************************")
+print()
 
 for flac_filename in Path(flac_watchfolder).rglob('*.flac'):
-    base = os.path.splitext(flac_filename)[0]
-    mp3_file = base + '.mp3'
-    flac_file = base + '.flac'        
-    command = "flac --decode --stdout " + str(flac_file) + " | lame --preset extreme - " + str(mp3_file)
+    # Filename without path and extension
+    base_filename = os.path.basename(os.path.splitext(flac_filename)[0])
+
+    # Get subfolder structure when generate mp3 files to new path
+    dirname = os.path.dirname(flac_filename) + '/'
+    flac_subfolder = dirname.replace(flac_watchfolder, '')    
+    mp3_output_folder = output_folder + flac_subfolder
     
-    print("Running command: " + command)
+    # Actual filename (with path) for mp3 file
+    mp3_file = mp3_output_folder + base_filename + '.mp3'
+
+    print("Input FLAC file: " + str(flac_filename))
+    print("Output MP3 file: " + str(mp3_file))
+    print()
+
+    # Make the path if it does not exists
+    Path(mp3_output_folder).mkdir(parents=True, exist_ok=True)
+
+    # Decode FLAC and pass it to LAME
+    command = "flac --decode --stdout " + str(flac_filename) + " | lame --preset extreme - " + str(mp3_file)
+    print("Executing command: " + command)    
     subprocess.run([command], shell=True)    
+    print('\n' * 3)
